57fe2b15ffdb86d40f3185f87100884e7986da95
unknown <nami0879@naver.com>
2025-01-15 21:03:19 +0900

    Save commit before impl instacedMesh

--- src/module/core/particle/ExplosionParticle.ts
+++ src/module/core/particle/ExplosionParticle.ts
@@ -1,131 +1,103 @@
 import * as THREE from 'three'
 
+import Particle from './Particle'
 import BaseParticle from "./BaseParticle"
 import Coordinates from "../../../type/Coordinates"
 import TYPE from '../../../definition/type'
 import ChildDustInfo from '../../../type/ChildDustInfo'
+import POINT from '../../../definition/point'
 
-export default class ExplosionParticle extends BaseParticle {
-    private endRelativeYPointStorage: number[] = []   // Updating the y-endPoint to affect the particle's rotation.
-    private pointStorage: Coordinates[]
-    // private trailStorage: THREE.Vector3[]
-    // private trail: THREE.Line
+export default class ExplosionParticle implements Particle {
+    public instancedMesh: THREE.InstancedMesh
+    private object3D: THREE.Object3D
+
+    private totalFrames: number
+    private remainingFrames: number
+    private elapsedFrames: number
 
     private constructor(
         currentAbsolutePoint: Coordinates,
-        endRelativePoint: Coordinates,
         explosionType: string,
         color: string
     ) {
         const time = 5
+        this.totalFrames = time * 60
+        this.remainingFrames = this.totalFrames
+        this.elapsedFrames = 0
+
         const radius = 0.05
         const segment = 8
         const geometry = new THREE.SphereGeometry(radius, segment, segment)
         const material = new THREE.MeshStandardMaterial({ color: color, transparent: true })
-        const childDustInfo: ChildDustInfo = {
-            use: true,
-            unit: 20,
-            request: false
-        }
-        if (explosionType === TYPE.EXPLOSION.ROUTINE.BURST) childDustInfo.unit = 10
-        else if (explosionType === (TYPE.EXPLOSION.FINALE.HUGE_BURST || TYPE.EXPLOSION.FINALE.CHAIN_BURST)) childDustInfo.use = false
-        super(currentAbsolutePoint, endRelativePoint, explosionType, color, time, geometry, material, childDustInfo)
 
-        this.endRelativeYPointStorage = new Array(this.getTotalFrames())
-        this.pointStorage = new Array(this.getTotalFrames()).fill(null).map(() => ({ x: 0, y: 0, z: 0 }))
-        // this.trailStorage = []
-        // const trailGeometry = new THREE.BufferGeometry().setFromPoints(this.trailStorage)
-        // const trailMaterial = new THREE.LineBasicMaterial({ color: this.getColor() })
-        // this.trail = new THREE.Line(trailGeometry, trailMaterial)
-
-        const copyedStartAbsolutePoint_y = this.getCurrentAbsolutePoint().y
-        let copyedEndRelativePoint_y = this.getEndRelativePoint().y
-        const gravity = 0.03
-        for (let i = 0; i < this.getTotalFrames(); i++) {
-            const easeOutFactor = super.getEaseOutFactor(i)
-            
-            this.pointStorage[i].x = this.getCurrentAbsolutePoint().x + (this.getEndRelativePoint().x * easeOutFactor) 
-
-            this.endRelativeYPointStorage[i] = copyedEndRelativePoint_y
-            this.pointStorage[i].y = copyedStartAbsolutePoint_y + copyedEndRelativePoint_y * easeOutFactor
-            copyedEndRelativePoint_y -= gravity
-
-            this.pointStorage[i].z = this.getCurrentAbsolutePoint().z + (this.getEndRelativePoint().z * easeOutFactor) 
-        }
+        this.instancedMesh = new THREE.InstancedMesh(geometry, material, POINT.EXPLOSION_OFFSET.BLOOM.length)
+        this.object3D = new THREE.Object3D()
     }
 
     public static create(
         currentAbsolutePoint: Coordinates,
-        endRelativePoint: Coordinates,
         explosionType: string,
         color: string
     ): ExplosionParticle {
-        return new ExplosionParticle(currentAbsolutePoint, endRelativePoint, explosionType, color)
+        return new ExplosionParticle(currentAbsolutePoint, explosionType, color)
     }
 
     public recycle(
         currentAbsolutePoint: Coordinates,
-        endRelativePoint: Coordinates,
         explosionType: string,
         color: string
     ): void {
-        super.setCurrentAbsolutePoint(currentAbsolutePoint)
-        super.setEndRelativePoint(endRelativePoint)
-        super.setExplosionType(explosionType)
-        super.setRemainingFrames(this.getTotalFrames())
-        super.setElapsedFrames(0)
-        super.setColor(color)
-        this.pointStorage = new Array(this.getTotalFrames()).fill(null).map(() => ({ x: 0, y: 0, z: 0 }))
-        // this.trailStorage = []
-        // const trailGeometry = new THREE.BufferGeometry().setFromPoints(this.trailStorage)
-        // const trailMaterial = new THREE.LineBasicMaterial({ color: this.getColor() })
-        // this.trail = new THREE.Line(trailGeometry, trailMaterial)
         
-        this.endRelativeYPointStorage = new Array(this.getTotalFrames())
-        this.pointStorage = new Array(this.getTotalFrames()).fill(null).map(() => ({ x: 0, y: 0, z: 0 }))
-        const copyedStartAbsolutePoint_y = this.getCurrentAbsolutePoint().y
-        let copyedEndRelativePoint_y = this.getEndRelativePoint().y
-        const gravity = 0.03
-        for (let i = 0; i < this.getTotalFrames(); i++) {
-            const easeOutFactor = super.getEaseOutFactor(i)
-            
-            this.pointStorage[i].x = this.getCurrentAbsolutePoint().x + (this.getEndRelativePoint().x * easeOutFactor)
-
-            this.endRelativeYPointStorage[i] = copyedEndRelativePoint_y
-            this.pointStorage[i].y = copyedStartAbsolutePoint_y + copyedEndRelativePoint_y * easeOutFactor
-            copyedEndRelativePoint_y -= gravity
-
-            this.pointStorage[i].z = this.getCurrentAbsolutePoint().z + (this.getEndRelativePoint().z * easeOutFactor)
-        }
     }
 
     public update(): void {
-        this.getCurrentAbsolutePoint().x = this.pointStorage[this.getElapsedFrames()].x
-        this.getCurrentAbsolutePoint().y = this.pointStorage[this.getElapsedFrames()].y
-        this.getCurrentAbsolutePoint().z = this.pointStorage[this.getElapsedFrames()].z
-        this.getEndRelativePoint().y = this.endRelativeYPointStorage[this.getElapsedFrames()]
+        for (let i = 0; i < POINT.EXPLOSION_OFFSET.BLOOM.length; i++) {
+            // 기존.
+            this.instancedMesh.getMatrixAt(i, this.object3D.matrix)
+            this.object3D.matrix.decompose(this.object3D.position, this.object3D.quaternion, this.object3D.scale)
 
-        this.position.set(this.getCurrentAbsolutePoint().x, this.getCurrentAbsolutePoint().y, this.getCurrentAbsolutePoint().z)
-        // Call if ExplosionParticle's shape is rect.
-        // super.rotateTowardsEndPoint()
+            const easeOutFactor = this.getEaseOutFactor(this.elapsedFrames)
 
-        // const trailPos = new THREE.Vector3(this.pointStorage[this.getElapsedFrames()].x, this.pointStorage[this.getElapsedFrames()].y, this.pointStorage[this.getElapsedFrames()].z)
-        // this.trailStorage.push(trailPos)
+            this.object3D.position.x += this.object3D.position.x + POINT.EXPLOSION_OFFSET.BLOOM[i].x * easeOutFactor
+            
+            const gravity = 0.03
+            this.object3D.position.y += this.object3D.position.y + POINT.EXPLOSION_OFFSET.BLOOM[i].y * easeOutFactor - gravity
 
-        // if (this.trailStorage.length > 10) this.trailStorage.shift()
-
-        // const trailGeometry = new THREE.BufferGeometry().setFromPoints(this.trailStorage)
-        // const trailMaterial = new THREE.LineBasicMaterial({ 
-        //     color: this.getColor(),
-        //     transparent: true,
-        //     opacity: (this.getRemainingFrames() / this.getTotalFrames()) / 2
-        // })
-        // this.trail = new THREE.Line(trailGeometry, trailMaterial)
-
-        super.update()
+            this.object3D.position.z += this.object3D.position.z + POINT.EXPLOSION_OFFSET.BLOOM[i].z * easeOutFactor
+            
+            this.object3D.updateMatrix()
+            this.instancedMesh.setMatrixAt(i, this.object3D.matrix)
+        }
     }
 
-    // public getTrail(): THREE.Line {
-    //     return this.trail
-    // }
+    public getElapsedRate(elapsedFrames: number = this.elapsedFrames) {
+        return elapsedFrames / this.totalFrames
+    }
+
+    protected getEaseOutFactor(elapsedFrames: number) {
+        const elapsedRate = this.getElapsedRate(elapsedFrames)
+        const easeOutFactor = 1 - (1 - elapsedRate) ** 4
+        // const easeInFactor = elapsedRate ** 4
+        return easeOutFactor
+    }
+
+    public getRemainingFrames(): number {
+        return this.remainingFrames
+    }
+
+    getCurrentAbsolutePoint(): Coordinates {
+        return {x:1,y:1,z:1}
+    }
+    getEndRelativePoint(): Coordinates {
+        return {x:1,y:1,z:1}
+    }
+    getExplosionType(): string {
+        return 'a'
+    }
+    getColor(): string {
+        return 'a'
+    }
+    getDustRequestStatus(): boolean {
+        return false
+    }
 }
